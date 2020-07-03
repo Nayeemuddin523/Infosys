40b9bf3d5a98c3a3e1b34f0e3a199beb6c8338f2
Nayeemuddin523 <52380307+Nayeemuddin523@users.noreply.github.com>
2020-07-03 16:30:34 +0530

    Initial commit

---
+++ TravelSpot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TravelSpot
+//
+//  Created by Mahesh Mac on 02/07/20.
+//  Copyright © 2020 Mahesh Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
